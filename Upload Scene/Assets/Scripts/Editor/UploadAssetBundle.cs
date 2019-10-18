5e8a7e36bf1bfe046ca74f585cfbe39a88ebd971
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-10-18 18:19:11 +0200

    [Add] Dialog window for uploading asset bundles

---
+++ Upload Scene/Assets/Scripts/Editor/UploadAssetBundle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class UploadAssetBundle : EditorWindow
+    {
+        // Add menu item named "My Window" to the Window menu
+        [MenuItem("Window/Upload Scene to Moodle Server")]
+        public static void ShowWindow()
+        {
+            //Show existing window instance. If one doesn't exist, make one.
+            EditorWindow.GetWindow(typeof(UploadAssetBundle));
+        }
+
+    }
+}
