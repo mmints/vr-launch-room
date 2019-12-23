e7d8fc9595be3d9994cbc4724897b6f6220a843e
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-12-23 16:58:15 +0100

    [Update] Use variables to load scene from server

--- VR Launch Room/Assets/Scripts/SceneBundleLoader.cs
+++ VR Launch Room/Assets/Scripts/SceneBundleLoader.cs
@@ -5,14 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneBundleLoader : MonoBehaviour
 {
+    public string sceneName;
+
     public IEnumerator Start()
     {
         // Download compressed Scene. If version 5 of the file named "Streamed-Level1.unity3d" was previously downloaded and cached.
         // Then Unity will completely skip the download and load the decompressed Scene directly from disk.
-        
-        // var download = UnityWebRequestAssetBundle.GetAssetBundle("http://141.26.178.164:666/Streamed-level.unity3d");
-        var download = UnityWebRequestAssetBundle.GetAssetBundle("http://141.26.140.219:666/Streamed-level.unity3d");
-        // var download = UnityWebRequestAssetBundle.GetAssetBundle("http://141.26.178.164:666/scenes");
+        var download = UnityWebRequestAssetBundle.GetAssetBundle("http://141.26.140.219:666/" + sceneName);
 
         // Both variations works!
 
@@ -33,8 +32,9 @@ public class SceneBundleLoader : MonoBehaviour
     public void LoadScene()
     {
         // Load the level we have just downloaded
-        SceneManager.LoadScene("Dummy");
-        var scene = SceneManager.GetSceneByName("Dummy");
+        // TODO: Do not use hard coded name strings!
+        SceneManager.LoadScene(sceneName);
+        var scene = SceneManager.GetSceneByName(sceneName);
         Debug.Log("Load scene: " + scene.name);
     }
 }
