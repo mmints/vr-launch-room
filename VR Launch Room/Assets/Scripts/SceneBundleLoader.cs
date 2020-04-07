52519b2e3654f29677b56802ff56e7d7b6183cc1
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-04-07 10:44:25 +0200

    [Update] Add variables for assetBundleName and starSceneName

--- VR Launch Room/Assets/Scripts/SceneBundleLoader.cs
+++ VR Launch Room/Assets/Scripts/SceneBundleLoader.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneBundleLoader : MonoBehaviour
 {
-    public string sceneName;
+    public string assetBundleName;
+    public string startSceneName;
 
     public IEnumerator Start()
     {
         // Download compressed Scene. If version 5 of the file named "Streamed-Level1.unity3d" was previously downloaded and cached.
         // Then Unity will completely skip the download and load the decompressed Scene directly from disk.
-        var download = UnityWebRequestAssetBundle.GetAssetBundle("http://141.26.140.219:666/" + sceneName);
+        var download = UnityWebRequestAssetBundle.GetAssetBundle("http://141.26.140.219:666/" + assetBundleName);
 
         // Both variations works!
 
@@ -27,14 +28,16 @@ public class SceneBundleLoader : MonoBehaviour
         // In order to make the Scene available from LoadLevel, we have to load the asset bundle.
         // The AssetBundle class also lets you force unload all assets and file storage once it is no longer needed.
         var bundle = DownloadHandlerAssetBundle.GetContent(download);
+        
+        Debug.Log("Download Complected");
     }
 
     public void LoadScene()
     {
         // Load the level we have just downloaded
         // TODO: Do not use hard coded name strings!
-        SceneManager.LoadScene(sceneName);
-        var scene = SceneManager.GetSceneByName(sceneName);
+        SceneManager.LoadScene(startSceneName);
+        var scene = SceneManager.GetSceneByName(startSceneName);
         Debug.Log("Load scene: " + scene.name);
     }
 }
