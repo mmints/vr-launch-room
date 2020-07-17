95ca68dc3cbc840238b84e04f062a73d1b285498
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-07-17 15:36:35 +0200

    [Fix] Unload assetBundle afer loading the scene

--- VR Launch Room/Assets/Scripts/SceneBundleLoader.cs
+++ VR Launch Room/Assets/Scripts/SceneBundleLoader.cs
@@ -8,14 +8,13 @@ public class SceneBundleLoader : MonoBehaviour
     public string assetBundleName;
     public string startSceneName;
 
+    private AssetBundle bundle;
+    
     public IEnumerator Start()
     {
         // Download compressed Scene. If version 5 of the file named "Streamed-Level1.unity3d" was previously downloaded and cached.
         // Then Unity will completely skip the download and load the decompressed Scene directly from disk.
         var download = UnityWebRequestAssetBundle.GetAssetBundle("http://141.26.140.219:666/" + assetBundleName);
-
-        // Both variations works!
-
         yield return download.SendWebRequest();
 
         // Handle error
@@ -27,17 +26,16 @@ public class SceneBundleLoader : MonoBehaviour
 
         // In order to make the Scene available from LoadLevel, we have to load the asset bundle.
         // The AssetBundle class also lets you force unload all assets and file storage once it is no longer needed.
-        var bundle = DownloadHandlerAssetBundle.GetContent(download);
-        
+        // Hack, but it works.
+        bundle = DownloadHandlerAssetBundle.GetContent(download);
         Debug.Log("Download Complected");
     }
 
-    public void LoadScene()
+    public void LoadScene() // Will be called by ButtonDown function of the top game object
     {
-        // Load the level we have just downloaded
-        // TODO: Do not use hard coded name strings!
         SceneManager.LoadScene(startSceneName);
         var scene = SceneManager.GetSceneByName(startSceneName);
         Debug.Log("Load scene: " + scene.name);
+        bundle.Unload(false);
     }
 }
