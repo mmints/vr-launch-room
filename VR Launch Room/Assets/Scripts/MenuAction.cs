5a2a888b72bf9fe241a875d3d76e0434c9f82359
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-07-17 14:52:06 +0200

    [Add] Action Script that reloads the vr launch room on pressing the menu button on the controller

---
+++ VR Launch Room/Assets/Scripts/MenuAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR;
+
+public class MenuAction : MonoBehaviour
+{
+
+    // a reference to the action
+    public SteamVR_Action_Boolean openMenu; //Currently just used to get back to the Launch Room
+    
+    public SteamVR_Input_Sources handType;
+
+    public string launchRoom;
+    
+    private void Start()
+    {
+        openMenu.AddOnStateDownListener(OnMenuButton, handType);
+    }
+    
+    public void OnMenuButton(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Menu Button Clicked!");
+        SceneManager.LoadScene(launchRoom);
+    }
+}
