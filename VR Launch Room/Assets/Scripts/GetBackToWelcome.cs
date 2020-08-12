cf5b5925fd3adfb1d7b01fda749f3de30d309625
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-08-12 18:08:54 +0200

    [Add] Simple script for getting back to the welcome room

---
+++ VR Launch Room/Assets/Scripts/GetBackToWelcome.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Simple Script for getting back to the welcome scene by pressing 'W'
+public class GetBackToWelcome : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Debug.Log("Return to Welcome Scene");
+            SceneManager.LoadScene("Welcome");
+        }
+    }
+}
