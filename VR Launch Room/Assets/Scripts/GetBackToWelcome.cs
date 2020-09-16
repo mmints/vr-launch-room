7e57fbcab6f2841ea655ed5bfad623a454a6903c
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-09-16 11:56:33 +0200

    [Update] Change key binding for Reset

--- VR Launch Room/Assets/Scripts/GetBackToWelcome.cs
+++ VR Launch Room/Assets/Scripts/GetBackToWelcome.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// Simple Script for getting back to the welcome scene by pressing 'W'
+// Simple Script for getting back to the welcome scene by pressing 'B'
 public class GetBackToWelcome : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.W))
+        if (Input.GetKeyUp(KeyCode.B))
         {
             Debug.Log("Return to Welcome Scene");
             SceneManager.LoadScene("Welcome");
