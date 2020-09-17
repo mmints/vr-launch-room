7798215f0f8563aa997b8fc41aece6b0d2d2ccaa
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-09-17 14:19:41 +0200

    [Add] Simple scenario of 2 scenes where it is possible to switch from one to the other

---
+++ VR Launch Room/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+
+/*
+ * This is a very simplified script. It checks hardcoded game object names.
+ * It is currently only used for testing purposes.
+ */
+
+public class SceneHandler : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+    public GameObject placeHolder;
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was clicked");
+        } else if (e.target.name == "Button")
+        {
+            placeHolder.SetActive(true);
+            Debug.Log("Button was clicked");
+        }
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }
+    }
+}
