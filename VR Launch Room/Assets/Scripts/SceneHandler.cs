d010d7a7d27fcb80c7a2d060ef134662a7a8d8da
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-09-30 09:30:26 +0200

    [Merge] Resolve merge conflict in SceneHandler.cs

--- VR Launch Room/Assets/Scripts/SceneHandler.cs
+++ VR Launch Room/Assets/Scripts/SceneHandler.cs
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-=======
-﻿/* SceneHandler.cs*/
 using System.Collections;
->>>>>>> bdc65648a13258d04d80c8a2391241ff589364df
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Valve.VR.Extras;
 
-<<<<<<< HEAD
 /*
  * This is a very simplified script. It checks hardcoded game object names.
  * It is currently only used for testing purposes.
@@ -20,11 +14,6 @@ public class SceneHandler : MonoBehaviour
 {
     public SteamVR_LaserPointer laserPointer;
     public GameObject placeHolder;
-=======
-public class SceneHandler : MonoBehaviour
-{
-    public SteamVR_LaserPointer laserPointer;
->>>>>>> bdc65648a13258d04d80c8a2391241ff589364df
 
     void Awake()
     {
@@ -40,10 +29,7 @@ public class SceneHandler : MonoBehaviour
             Debug.Log("Cube was clicked");
         } else if (e.target.name == "Button")
         {
-<<<<<<< HEAD
             placeHolder.SetActive(true);
-=======
->>>>>>> bdc65648a13258d04d80c8a2391241ff589364df
             Debug.Log("Button was clicked");
         }
     }
