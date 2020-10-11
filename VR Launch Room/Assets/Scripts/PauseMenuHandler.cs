92944d6e3095b2901da6baf67edcf1509912a489
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-10-11 20:21:21 +0200

    [Update] Menu only rotates along Y axis (up)

--- VR Launch Room/Assets/Scripts/PauseMenuHandler.cs
+++ VR Launch Room/Assets/Scripts/PauseMenuHandler.cs
@@ -10,6 +10,10 @@ using Valve.VR;
 
 public class PauseMenuHandler : MonoBehaviour
 {
+    private float tilt = 5f;
+    private float height = -0.5f;
+    private float distance = 1.25f;
+    
     public SteamVR_Action_Boolean openMenu;
     public SteamVR_Input_Sources handType;
     
@@ -63,8 +67,14 @@ public class PauseMenuHandler : MonoBehaviour
     // Works just like the ingame menu in SteamVR Home
     void Pause()
     {
-        pauseMenuUI.transform.rotation = cameraTransform.rotation;
-        pauseMenuUI.transform.position = cameraTransform.position + (cameraTransform.forward * 3f);
+        pauseMenuUI.transform.localEulerAngles = new Vector3(tilt, cameraTransform.localEulerAngles.y, 0f);
+
+        // Move the menu to the position of the player and transform it in front of it
+        var position = pauseMenuUI.transform.position;
+        position = cameraTransform.position + (cameraTransform.forward * distance);
+        position = new Vector3(position.x, cameraTransform.transform.position.y + height, position.z);
+        pauseMenuUI.transform.position = position;
+        
         pauseMenuUI.SetActive(true);
         gameIsPaused = true;
     }
