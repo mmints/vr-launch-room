1e6a765ebb7126c3696514e9268a27eb3415e513
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-10-13 06:50:55 +0200

    [Add] Script to trigger settings in Steam VR

---
+++ VR Launch Room/Assets/Scripts/SteamVRSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+// Use this script to trigger options and setting in Steam VR
+public class SteamVRSettings : MonoBehaviour
+{
+    public bool enableTeleportHint = false;
+    
+    void Start()
+    {
+        if (!enableTeleportHint)
+            Teleport.instance.CancelTeleportHint (); // Disable teleportation hint
+    }
+}
