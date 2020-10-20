c21d154ed98d6a30682d60b1fcb3fd3ed4eb54bd
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-10-20 22:33:57 +0200

    [Add] Adapter script for SteamVR_LoadLevel.cs

---
+++ VR Launch Room/Assets/Scripts/SceneLoaderVR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script adapts the scene loading functionality from SteamVR_LoadLevel.cs (SteamVR 2.0).
+ * 
+ * SteamVR_LoadLevel.cs is useful to create smooth transitions between levels.
+ * It can be extended with additional information to the user like loading bar
+ * or title text etc.
+ */
+
+public class SceneLoaderVR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
