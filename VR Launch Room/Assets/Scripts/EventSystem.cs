012fea53f6b9edb6010539ce8cb089692ba1edc9
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-10-21 01:39:15 +0200

    [Add] EventSystem script for managing a custom event system

---
+++ VR Launch Room/Assets/Scripts/EventSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class EventSystem : MonoBehaviour
+{
+    public static EventSystem current; // Current Event
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public event Action<string> loadLevel;
+
+    public void LoadLevel(string levelName)
+    {
+        if (loadLevel != null)
+        {
+            loadLevel(levelName);
+            Debug.Log("EventSystem: Button was Triggered!");
+        }
+    }
+}
