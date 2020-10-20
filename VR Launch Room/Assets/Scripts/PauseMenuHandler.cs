7296b64b529008fb569d7e4ab0a0e0eb5a602ca3
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-10-20 23:04:20 +0200

    [Impl] Integrate laser pointer controlling into the pause menu handler script

--- VR Launch Room/Assets/Scripts/PauseMenuHandler.cs
+++ VR Launch Room/Assets/Scripts/PauseMenuHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.Extras;
 
 /*
  * This script handles the appearance of the pause menu.
@@ -10,38 +11,31 @@ using Valve.VR;
 
 public class PauseMenuHandler : MonoBehaviour
 {
+    // Spawning position of the UI Canvas
     private float tilt = 5f;
     private float height = -0.5f;
     private float distance = 1.25f;
     
-    public SteamVR_Action_Boolean openMenu;
-    public SteamVR_Input_Sources handType;
-    
-    public Transform cameraTransform;
-    public GameObject pauseMenuUI; // Game object, that contains a canvas with the GUI elements
-
+    // Controller options
+    public SteamVR_Action_Boolean openMenu;   // SteamVR Controller Action
+    public SteamVR_Input_Sources handType;    // The Hand (Controller) that should trigger the Action
     // TODO: Implement the appearance and disappearance of the laser pointer
-    // public GameObject laserPointer; // Game object that contains the component: SteamVR Laser Pointer (Script)
+    public SteamVR_LaserPointer laserPointer; // The Hand (Controller) that has the SteamVR_LaserPointer.cs Script
     
-    public static bool gameIsPaused = false; // Indicates if the menu is currently open or not
-
-    void Start()
+    public Transform cameraTransform;  // World Space Position of the Players Head to Calculate the View Direction
+    public GameObject pauseMenuUI;     // Game object, that contains a canvas with the GUI elements
+    
+    // TODO: Turn gameIsPaused to public to use it outside as trigger for pause functions
+    private bool gameIsPaused = false; // Indicates if the menu is currently open or not
+    
+    // TODO: Change for now hard coded names to dynamic public variables
+    private string scene1 = "Scene1";
+    private string scene2 = "Scene2";
+    
+    void Awake()
     {
         openMenu.AddOnStateDownListener(OnMenuButton, handType);
-    }
-    
-    void OnMenuButton(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        Debug.Log("Menu Button Clicked!");
-
-        if (gameIsPaused)
-        {
-            Resume();
-        }
-        else
-        {
-            Pause();
-        }
+        laserPointer.PointerClick += PointerClick;
     }
     
     void Update()
@@ -60,13 +54,26 @@ public class PauseMenuHandler : MonoBehaviour
         }
     }
     
-    void Resume()
+    // Open Menu on MenuButton click on the Controller
+    void OnMenuButton(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        pauseMenuUI.SetActive(false);
-        //laserPointer.SetActive(false);
-        gameIsPaused = false;
+        Debug.Log("Menu Button Clicked!");
+
+        if (gameIsPaused)
+            Resume();
+        else
+            Pause();
     }
 
+    // Use the laserPointer and the Trigger of the controller to enable the transition
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == scene1)
+            Debug.Log("Scene1 was clicked");
+        else if (e.target.name == scene2)
+            Debug.Log("Scene2 was clicked");
+    }
+    
     // Opens a pause menu at a fixed position in the world relatively to the head/camera position
     // Works just like the ingame menu in SteamVR Home
     void Pause()
@@ -74,13 +81,18 @@ public class PauseMenuHandler : MonoBehaviour
         pauseMenuUI.transform.localEulerAngles = new Vector3(tilt, cameraTransform.localEulerAngles.y, 0f);
 
         // Move the menu to the position of the player and transform it in front of it
-        var position = pauseMenuUI.transform.position;
-        position = cameraTransform.position + (cameraTransform.forward * distance);
+        var position = cameraTransform.position + (cameraTransform.forward * distance);
         position = new Vector3(position.x, cameraTransform.transform.position.y + height, position.z);
         pauseMenuUI.transform.position = position;
         
         pauseMenuUI.SetActive(true);
-        //laserPointer.SetActive(true);
         gameIsPaused = true;
+    }
+    
+    // Close the Pause Menu and get back to the game.
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        gameIsPaused = false;
     }
 }
