fe559d137121c2171ea2a3bb0a75153b53bdddb4
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-03-17 23:49:45 +0100

    [Add] Data object that can be deserialized from json

---
+++ VR Launch Room/Assets/Scripts/Level.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class Level
+{
+    public string name;
+    public string displayName;
+    public string assetBundleURL;
+    public string thumbnailBase64;
+
+    // Constructor that deserialize a json to a Level object
+    public Level(string pathToJson)
+    {
+        string json = File.ReadAllText(pathToJson); // TODO (POC): get this data from moodle connector
+        Level level = JsonUtility.FromJson<Level>(json);
+
+        this.name = level.name;
+        this.displayName = level.displayName;
+        this.assetBundleURL = level.assetBundleURL;
+        this.thumbnailBase64 = level.thumbnailBase64;
+    }
+}
