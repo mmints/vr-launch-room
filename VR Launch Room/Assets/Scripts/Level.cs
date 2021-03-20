540fd82f6739634e2f64298b0acaf25c5c07d39b
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-03-20 18:07:40 +0100

    [Add] Base64 Decoding of the Thumbnail

--- VR Launch Room/Assets/Scripts/Level.cs
+++ VR Launch Room/Assets/Scripts/Level.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 public class Level
@@ -9,14 +10,29 @@ public class Level
     public string thumbnailBase64;
 
     // Constructor that deserialize a json to a Level object
-    public Level(string pathToJson)
+    public Level(string json)
     {
-        string json = File.ReadAllText(pathToJson); // TODO (POC): get this data from moodle connector
         Level level = JsonUtility.FromJson<Level>(json);
 
         this.name = level.name;
         this.displayName = level.displayName;
         this.assetBundleURL = level.assetBundleURL;
-        this.thumbnailBase64 = level.thumbnailBase64;
+        
+        // Remove redundant data information from base64 encodeing before the ',' -> data:image/png;base64
+        string[] subs = level.thumbnailBase64.Split(',');
+        this.thumbnailBase64 = subs[1];
+    }
+
+    
+    // Returns the encoded thumbnail image as a Texture2D.
+    // This is used to display the image on RawImage Object on a GUI Canvas.
+    public Texture2D GetImage()
+    {
+        byte[] bytes = Convert.FromBase64String(thumbnailBase64);
+        Texture2D tex = new Texture2D(300,200);
+        Debug.Log("Decoded Image loaded to Texture2D.");
+        
+        tex.LoadImage(bytes);
+        return tex;
     }
 }
