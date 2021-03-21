e5191637aa8a185f103a21df9c2005b01446d805
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-03-21 18:10:54 +0100

    [Add] Get Request of Asset Bundles

--- VR Launch Room/Assets/Scripts/NetworkManager.cs
+++ VR Launch Room/Assets/Scripts/NetworkManager.cs
@@ -1,48 +1,76 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 
+// This Class manages the connection to the odl4u.ko-ld.de server.
+// Level information and the corresponding AssetBundles are
+// downloaded and made available to the rest of the software.
+
 public class NetworkManager : MonoBehaviour
 {
+    // Address to odl4u.ko-ld.de.
     private string _url = "http://141.26.140.219:666/";
     
-
+    private Level _level;
+    private AssetBundle _assetBundle;
     
-    public void GetLevelJson(string levelName)
+    // Use this inside of a Co-Routine an safe the output in a member variable.
+    // Call this always after GetLevelJson!
+    public Level GetLevel()
     {
-        StartCoroutine(GetRequest(_url + "/json/" + levelName + ".json", (UnityWebRequest req) =>
+        return _level;
+    }
+
+    public AssetBundle GetAssetBundle()
+    {
+        return _assetBundle;
+    }
+
+    public IEnumerator GetLevelRequest(string levelName)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(_url + "/json/" + levelName + ".json"))
         {
-            if (req.isNetworkError || req.isHttpError)
+            // Send the request and wait for a response
+            yield return request.SendWebRequest();
+            
+            if (request.isNetworkError || request.isHttpError)
             {
-                Debug.LogError($"{req.error}: {req.downloadHandler.text}");
+                Debug.LogError($"{request.error}: {request.downloadHandler.text}");
             } else
             {
-                Debug.Log(req.downloadHandler.text);
-                
+                Debug.Log(request.downloadHandler.text);
+            
                 // The issue seems to be that there are exactly 3 extra bytes on the head of the response.
                 // The fix is to use req.bytes instead of req.text, then slice off the extra 3 bytes.
                 // From: https://forum.unity.com/threads/jsonutility-fromjson-error-invalid-value.421291/
                 string jsonString;
                 jsonString = System.Text.Encoding.UTF8.
-                    GetString(req.downloadHandler.data, 3, req.downloadHandler.data.Length - 3);
+                    GetString(request.downloadHandler.data, 3, request.downloadHandler.data.Length - 3);
 
-                Level level = new Level(jsonString);
-                Debug.Log("Level Display Name: " + level.displayName);
+                _level = new Level(jsonString);
+                Debug.Log("Instantiate Level from Json: " + _level.name);
             }
-            
-        }));
+        }
     }
     
-    IEnumerator GetRequest(string url, Action<UnityWebRequest> callback)
+    public IEnumerator GetAssetBundleRequest(string assetBundleName)
     {
-        using (UnityWebRequest request = UnityWebRequest.Get(url))
+
+        using (UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(_url + "/asset-bundles/" + assetBundleName,0, 0))
         {
             // Send the request and wait for a response
             yield return request.SendWebRequest();
-            callback(request);
+        
+            // Handle error
+            if (request.isNetworkError || request.isHttpError) 
+            {
+                Debug.LogError($"{request.error}: {request.downloadHandler.text}");
+            }
+            else
+            {
+                _assetBundle = DownloadHandlerAssetBundle.GetContent(request);
+            }
         }
     }
 }
