5ac63778a214cb36714957f076392e929d6b1404
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-03-22 17:09:35 +0100

    [Add] GUI Elements for Editor to Serialize Levels and Build Asset Bundles

---
+++ VR Launch Room/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("DigiLab4U/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+                                        BuildAssetBundleOptions.None, 
+                                        BuildTarget.StandaloneWindows);
+    }
+}
---
+++ VR Launch Room/Assets/Editor/Serialization.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class Serialization
+{
+    [MenuItem("DigiLab4U/Serialize")]
+    public static void Serialize()
+    {
+        // Create Levels
+        // TODO: Create GUI to Serialize Selected Level
+        // -> Currently: Exchange the params in this call
+        Level level = new Level("name", "displayName", "path");
+        
+        string jsonDummyScene = JsonUtility.ToJson(level);
+
+        string jsonDir = "Assets/json";
+        if(!Directory.Exists(jsonDir))
+        {
+            Directory.CreateDirectory(jsonDir);
+        }
+        File.WriteAllText(jsonDir + "/DummyScene.json", jsonDummyScene, Encoding.UTF8); // TODO: Level Name
+    }
+}
